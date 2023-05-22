4cc71075820adfe23210c789df8d7886458f0f2f
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-05-22 10:50:52 -0700

    Update models.py

--- moviefinder/models.py
+++ moviefinder/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Movies(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    overview = models.TextField()
+    image = CloudinaryField('images')
