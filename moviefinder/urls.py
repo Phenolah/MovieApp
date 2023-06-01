6274df448b73095747f66d2d49139952cc386e4d
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-06-01 07:34:43 -0700

    Update urls.py

--- moviefinder/urls.py
+++ moviefinder/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.movie_by_genre, name='home'),
     path('search/', views.search, name='search'),
     path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('logout/', views.logout, name='logout'),
 
 
 ]
