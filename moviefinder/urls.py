7c2034c30747e669f344e08b9c735fea6b01f671
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-06-07 21:47:12 +0300

    Update urls.py

--- moviefinder/urls.py
+++ moviefinder/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.movie_by_genre, name='home'),
-    path('search/', views.search, name='search'),
     path('login/', views.login, name='login'),
     path('registration/', views.registration, name='registration'),
     path('logout/', views.logout, name='logout'),
