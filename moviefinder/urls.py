4e5293fc325ab18687f620b48b8d18cce58600c0
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-06-15 23:30:23 +0300

    Update urls.py

--- moviefinder/urls.py
+++ moviefinder/urls.py
@@ -1,10 +1,16 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.movie_by_genre, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('registration/', views.registration, name='registration'),
+    path("change-password/", auth_views.PasswordChangeView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('logout/', views.logout, name='logout'),
     path('moviedetails/', views.MoviedetailView.as_view(), name='moviedetails' )
 
