cc26ae3071fd3d9906428984c1b01af81f62288d
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-07-11 11:48:56 +0300

    Update urls.py

--- moviegenerator/urls.py
+++ moviegenerator/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('moviefinder.urls')),
+    path('api/auth', include('authentication.urls'))
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) )
