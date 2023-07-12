f547ad843e4b22b0bfb1bd094f3249af425861a7
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-07-12 15:16:34 +0300

    Update serializers.py

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField (max_length=70, min_length=8, write_only=True)
@@ -33,3 +34,34 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validate_data):
         instance.email = validate_data.get('email', instance.email)
         instance.password = validate_data.get('password', instance.password)
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        label='Username'
+    )
+    password = serializers.CharField(
+        label = 'Password',
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    #https://www.guguweb.com/2022/01/23/django-rest-framework-authentication-the-easy-way/#why-you-should-avoid-jwt-for-django-rest-framework-authentication
+
+    def validate(self,attrs):
+        # take username and password from request
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            # try to authenticate the user
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+            if not user:
+                #if we dont have a regular user, raise a Validationerror
+                msg= 'Access denied: wrong useername or password.'
+                raise serializers.ValidationError(msg, code='authorization')
+            else:
+                msg = 'Both username and password are required.'
+                raise serializers.ValidationError(msg, code='authorization')
+            attrs['user'] = user
+            return attrs
