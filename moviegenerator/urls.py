da3cb4800e04a4beb25bc784a14f69520acf620f
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-07-12 15:09:46 +0300

    Update urls.py

--- moviegenerator/urls.py
+++ moviegenerator/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('moviefinder.urls')),
-    path('api/auth', include('authentication.urls'))
+    path('api/auth', include('authentication.urls')),
+    #an endpoint for obtaining JWT tokens
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) )
